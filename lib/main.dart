cf5af2adb031682fe5a5761d028e09bdbba6f09a
wuhaolei <wuhl01@rd.netease.com>
2023-04-21 19:02:08 +0800

    feat: 添加单击lottie动画

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:codelab_vocabulary/screen/default_screen_display_helper.dart';
 import 'package:codelab_vocabulary/screen/screen_display_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,56 +27,88 @@ class VocabularyPage extends StatefulWidget {
   State<VocabularyPage> createState() => _VocabularyPageState();
 }
 
-class _VocabularyPageState extends State<VocabularyPage> {
-
+class _VocabularyPageState extends State<VocabularyPage>
+    with WidgetsBindingObserver, TickerProviderStateMixin{
   late ScreenDisplayHelper _playHelper;
+  late AnimationController _clickAnimationController;
+  Offset _clickPosition = Offset.zero;
 
   @override
   void initState() {
     _playHelper = DefaultPandaDisplayHelper();
+    _clickAnimationController = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 700),
+    );
     _playHelper.setScreenLandscape(context);
     _playHelper.hideSystemStatusBar(widget.toString());
     super.initState();
   }
+
+  @override
+  void dispose() {
+    _clickAnimationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            child: Image.asset(
-              "assets/images/bg_vocabulary.png",
-              fit: BoxFit.fill,
+      body: Listener(
+        onPointerDown: (details) {
+          setState(() {
+            _clickPosition = details.localPosition;
+          });
+          _clickAnimationController.reset();
+          _clickAnimationController.forward().whenComplete(() {
+            _clickAnimationController.reset();
+          });
+
+        },
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: Image.asset(
+                "assets/images/bg_vocabulary.png",
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            child: Image.asset(
-              "assets/images/bg_vocabulary_book_2.png",
-              fit: BoxFit.contain,
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: Image.asset(
+                "assets/images/bg_vocabulary_book_2.png",
+                fit: BoxFit.contain,
+              ),
             ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            child: Image.asset(
-              "assets/images/bg_vocabulary_book_1.png",
-              fit: BoxFit.contain,
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: Image.asset(
+                "assets/images/bg_vocabulary_book_1.png",
+                fit: BoxFit.contain,
+              ),
             ),
-          ),
-        ],
+            Positioned(
+                top: _clickPosition.dy - 20,
+                left: _clickPosition.dx - 20,
+                child: IgnorePointer(
+                  child: Lottie.asset("assets/lottie/click.json",
+                      width: 60, height: 60, repeat: false, controller: _clickAnimationController
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
 }
-
